b648fe40df4f1e7145aad0b3e4e0455a85a97561
Justine Gennaro <justine.rouzier@gmail.com>
2019-08-16 12:12:46 -0500

    added some style to the card component and imported some font-family in index.html

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -17,20 +17,11 @@ const App = () => {
 
   }, []);
 
-  
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      
         {people.map(info => {
-        return <CharacterCard name={info.name} height={info.height} gender={info.gender} birth_year={people.birth_year} />
+        return <CharacterCard name={info.name} height={info.height} gender={info.gender} birth_year={info.birth_year} hair_color={info.hair_color} eye_color={info.eye_color}/>
         })}
     </div>
   );
--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import styled from 'styled-components'; 
+import "./StarWars.css";
+
+const Container  = styled.div `
+    background-color: #73977A;
+    width: 300px;
+    border: 1px solid black;
+    margin: 10px;
+    border-radius: 10px;
+  `
+
+const NameStyle = styled.h2 `
+    color: #383F3A;
+    font-family: 'Lexend Tera', sans-serif;
+`
+
+const ParaStyle = styled.p `
+    font-family: 'Inconsolata', monospace;
+`
+
 
 function CharacterCard(props) {
     return (
         <div>
-            <h2>{props.name}</h2>
-            <img src={props.homeworld} alt="Homeworld"></img>
-            <p>Height: {props.height}</p>
-            <p>Birth year: {props.birth_year}</p>
-            <p>gender: {props.gender}</p>
+            <Container>
+                <NameStyle>{props.name}</NameStyle>
+                {/* <img src={props.homeworld} alt="Homeworld"></img> */}
+                <ParaStyle><strong>Height: </strong> {props.height}</ParaStyle>
+                <ParaStyle><strong>Birth year: </strong>{props.birth_year}</ParaStyle>
+                <ParaStyle><strong>Gender: </strong>{props.gender}</ParaStyle>
+                <ParaStyle><strong>Eye Color: </strong>{props.eye_color}</ParaStyle>
+                <ParaStyle><strong>Hair Color: </strong>{props.hair_color}</ParaStyle>
+            </Container>
         </div>
     )
 }
